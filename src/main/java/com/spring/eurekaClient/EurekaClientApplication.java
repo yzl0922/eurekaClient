814a87fc3c6c56de3bfe929389db724d7d5eeaa2
Yuanziliang <617018543@qq.com>
2021-04-25 10:45:14 +0800

    first

---
+++ src/main/java/com/spring/eurekaClient/EurekaClientApplication.java
@@ -0,0 +1,22 @@
+package com.spring.eurekaClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi " + name + ",i am from port:" + port;
+    }
+}
