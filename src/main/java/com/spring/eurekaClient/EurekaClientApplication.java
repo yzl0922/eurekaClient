00339507f6d1c629c798626b387fa408bb2d5000
Yuanziliang <617018543@qq.com>
2021-04-25 10:46:59 +0800

    first

--- src/main/java/com/spring/eurekaClient/EurekaClientApplication.java
+++ src/main/java/com/spring/eurekaClient/EurekaClientApplication.java
@@ -1,7 +1,12 @@
 package com.spring.eurekaClient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
